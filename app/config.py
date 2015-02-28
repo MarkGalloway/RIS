fc513cd2699e667839a9f5db17d251e064d88e3a
Mark Galloway <mgallowa@ualberta.ca>
2015-02-27 20:03:21 -0700

    Make things more Flasky

--- app/config.py
+++ app/config.py
@@ -1,3 +1,11 @@
+import os
+
 # WTForms Config
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'colin-is-cool'  # nobody will guess
+
+# Database Config
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'mysql:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
