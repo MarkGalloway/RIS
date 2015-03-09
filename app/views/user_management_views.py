acbfe6e7937cd17604e89034e50509cf2a894db7
Colin Hunt <colin.hunt@live.com>
2015-03-09 13:24:09 -0600

    Added ability to delete users from user list.

--- app/views/user_management_views.py
+++ app/views/user_management_views.py
@@ -1,11 +1,28 @@
+from datetime import date
 from flask import render_template, flash, redirect, url_for
 from app import app, db, models
 from app.forms.user_management import UserForm
+
 
 @app.route('/user_management')
 def user_management():
     users = models.User.query.all()
     return render_template('list_users.html', users=users)
+
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    form = UserForm()
+    if form.validate_on_submit():
+        user = models.User()
+        form.populate_obj(user)
+        user.date_registered = date.today()
+        db.session.add(user)
+        db.session.commit()
+        flash(u'Data has been saved for User {}'.format(form.user_name.data))
+        return redirect(url_for('user_management'))
+    return render_template('edit_user.html', form=form, actionName="Add")
+
 
 @app.route('/edit_user/<userName>', methods=['GET', 'POST'])
 def edit_user(userName):
@@ -22,4 +39,15 @@ def edit_user(userName):
         form.password.data = user.password
         form.user_class.data = user.user_class
         form.person_id.data = user.person_id
-    return render_template('edit_user.html', form=form)
+    return render_template('edit_user.html', form=form, actionName="Edit")
+
+@app.route('/delete_user/<userName>', methods=['GET', 'POST'])
+def delete_user(userName):
+    user = models.User.query.get_or_404(userName)
+    form = UserForm(obj=user)
+    if form.is_submitted():
+        db.session.delete(user)
+        db.session.commit()
+        flash(u'{} has been deleted'.format(userName))
+        return redirect(url_for('user_management'))
+    return render_template('delete_user.html', form=form)
