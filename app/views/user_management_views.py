27b2cbebc95bd8840e0672539fa09f96d3e27cd4
Colin Hunt <colin.hunt@live.com>
2015-03-09 12:39:28 -0600

    Added user management page to list users, and link to it on nav bar. Added link to edit users from the list.

--- app/views/user_management_views.py
+++ app/views/user_management_views.py
@@ -2,8 +2,13 @@ from flask import render_template, flash, redirect, url_for
 from app import app, db, models
 from app.forms.user_management import UserForm
 
+@app.route('/user_management')
+def user_management():
+    users = models.User.query.all()
+    return render_template('list_users.html', users=users)
+
 @app.route('/edit_user/<userName>', methods=['GET', 'POST'])
-def editUser(userName):
+def edit_user(userName):
     user = models.User.query.get_or_404(userName)
     form = UserForm(obj=user)
     if form.validate_on_submit():
@@ -11,7 +16,7 @@ def editUser(userName):
         form.populate_obj(user)
         db.session.commit()
         flash(u'Data has been saved for User {}'.format(form.user_name.data))
-        return redirect(url_for('index'))
+        return redirect(url_for('user_management'))
     else:
         form.user_name.data = user.user_name
         form.password.data = user.password
