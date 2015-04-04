c03d2cbe2baa943d558ae47070b0309e2068c71b
Mark Galloway <mgallowa@ualberta.ca>
2015-04-04 02:39:44 -0600

    Some image display fixes

--- app/config.py
+++ app/config.py
@@ -19,6 +19,6 @@ SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://ris_user:r1s_p4ssw0rd@45.56.81
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 # Upload stuffs
-# UPLOAD_FOLDER = '/Users/markgalloway/Documents/391/RIS/imgtest'
+# UPLOAD_FOLDER = '/Users/markgalloway/Documents/images'
 UPLOAD_FOLDER = '/home/mark/static/'
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB
--- app/views/util/image.py
+++ app/views/util/image.py
@@ -7,6 +7,6 @@ def resize_image(inp, outp, width=150, height=150):
     Resizes an image
     """
     img = Pil_Image.open(inp)
-    img.thumbnail((width, height), Pil_Image.ANTIALIAS)
-    # img = ImageOps.fit(img, (width, height), Pil_Image.ANTIALIAS)
+    # img.thumbnail((width, height), Pil_Image.ANTIALIAS)
+    img = ImageOps.fit(img, (width, height), Pil_Image.ANTIALIAS)
     img.save(outp, format='JPEG', quality=75)
